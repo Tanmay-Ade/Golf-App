40332071f0cd7049e3f212810e76f8cb312d6c28
Tanmay Ade <121955812+Tanmay-Ade@users.noreply.github.com>
2023-09-14 14:46:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,116 @@
+let crsr = document.querySelector("#cursor")
+let blur = document.querySelector("#cursor-blur")
+
+document.addEventListener("mousemove", function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+    blur.style.left = dets.x - 200 +"px"
+    blur.style.top = dets.y - 200 +"px"
+})
+
+let h4all = document.querySelectorAll("#nav h4")
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter", function(){
+        crsr.style.scale = 3
+        crsr.style.border = "1px solid #fff"
+        crsr.style.backgroundColor = "transparent"
+    })
+
+    elem.addEventListener("mouseleave", function(){
+        crsr.style.scale = 1
+        crsr.style.border = "1px solid #95c11e"
+        crsr.style.backgroundColor = "#95c11e"
+    })
+})
+
+gsap.to("#nav", {
+    backgroundColor: "#000",
+    height: "90px",
+    duration: 0.5,
+    scrollTrigger: {
+        trigger: "#nav",
+        scroller: "body",
+        // markers: true,
+        start: "top -11%",
+        scrub: 1
+    }
+})
+
+gsap.to("#main", {
+    backgroundColor: "#000",
+    scrollTrigger: {
+        trigger: "#main",
+        scroller: "body",
+        // markers:true,
+        start: "top -25%",
+        end: "top -70%",
+        scrub: 2
+    }
+})
+
+gsap.from("#about-us img, #about-us-in", {
+    y:90,
+    opacity:0,
+    duration:1,
+    scrollTrigger:{
+        trigger:"#about-us",
+        scroller: "body",
+        // markers:true,
+        start: "top 70%",
+        end: "top 65%",
+        scrub:2
+    }
+})
+
+gsap.from(".card", {
+    scale: 0.8,
+    opacity:0,
+    duration:1,
+    stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller: "body",
+        // markers:true,
+        start: "top 70%",
+        end: "top 65%",
+        scrub:2
+    }
+})
+
+gsap.from("#colon1", {
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller: "body",
+        // markers: true,
+        start:"top 55%",
+        end: "top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#colon2", {
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller: "body",
+        // markers: true,
+        start:"top 55%",
+        end: "top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#page4 h1", {
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller: "body",
+        // markers: true,
+        start:"top 100%",
+        end: "top 70%",
+        scrub:3
+    }
+})
